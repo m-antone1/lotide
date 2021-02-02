58dc24f924141a06e95dcdd4bc89d862b7f8c7ce
Michael Antone <mike_a1@live.com>
2021-02-02 01:27:02 +0000

    Asserting the head

--- head.js
+++ head.js
@@ -5,6 +5,9 @@ const assertEqual = function(actual, expected) {
   }
   console.assert(actual === expected, message);
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(actual) {
+  let answer = actual[0];
+  return answer;
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
