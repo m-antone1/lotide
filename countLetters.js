082f87245c81a5abdef4062e1887ed8baa12e458
Michael Antone <mike_a1@live.com>
2021-02-04 04:41:06 +0000

    Letter count using for...of

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion Failed: ${actual} !== ${expected} `);
+  }
+
+};
+const countLetters = function (letters) {
+  const result = {};
+  for(const letter of letters) {
+    if (letter !== '') {
+      if (result.hasOwnProperty(letter)) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  } return result;
+}
+console.log(countLetters('This will work now'));
