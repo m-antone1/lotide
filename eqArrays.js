dc4ee760534d5b20ee356c4304838286dd621a1e
Michael Antone <mike_a1@live.com>
2021-02-08 23:05:17 +0000

    eqArrays using modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let message = `🛑️🛑️🛑️${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(`Assertion Passed:✅️✅️✅️ ${actual} === ${expected}`);
-  }
-  console.assert(actual === expected, message);
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -21,8 +13,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); 
+module.exports = eqArrays;
