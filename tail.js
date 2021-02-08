ce77eb546b385c9c25d91cb4bfc19d1a42376ba4
Michael Antone <mike_a1@live.com>
2021-02-08 22:59:03 +0000

    refactored tail

--- tail.js
+++ tail.js
@@ -1,14 +1,6 @@
-const assertEqual = function(actual, expected) {
-  let message = `${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-  console.assert(actual === expected, message);
-};
+const assertEqual = require('./assertEqual');
 const tail = function(actual) {
-  let myTail = actual;
-  return myTail.tail;
+  return actual.slice(1);
 };
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+module.exports = tail;
