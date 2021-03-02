b0ad019d4183d1f92fabb6a07205dbf048d66a6d
Michael Antone <mike_a1@live.com>
2021-03-02 03:38:12 +0000

    head.js rebuilt from step one

--- head.js
+++ head.js
@@ -1,7 +1,26 @@
-const assertEqual = require('./assertEqual');
-const head = function(actual) {
-  let answer = actual[0];
-  return answer;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (myHead) {
+  return myHead[0];
 }
 
-module.exports = head;
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head(['one']), 1);
+
+
+// !!! THIS IS CODE FROM MY FIRST COHORT THAT I AM STORING FOR COMPARISON !!!
+// const assertEqual = require('./assertEqual');
+// const head = function(actual) {
+//   let answer = actual[0];
+//   return answer;
+// }
+
+// module.exports = head;
