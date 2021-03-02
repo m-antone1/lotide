e4022d10fa7540f8319383e50079a5dc369aaf5f
Michael Antone <mike_a1@live.com>
2021-03-02 03:06:53 +0000

    assertEqual take 2

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,27 @@
 const assertEqual = function(actual, expected) {
-  let message = `🛑️🛑️🛑️${actual} !== ${expected}`;
   if (actual === expected) {
-    console.log(`Assertion Passed:✅️✅️✅️ ${actual} === ${expected}`);
+    console.log(`✅️✅️✅️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion Failed: ${actual} !== ${expected} `);
   }
-  console.assert(actual === expected, message);
 };
 
+assertEqual("March", "cohort");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("fun", "Fun");
 
 
-module.exports = assertEqual;
+
+// !!! THIS IS CODE FROM MY FIRST COHORT THAT AM STORING FOR COMPARISON !!!
+// const assertEqual = function(actual, expected) {
+//   let message = `🛑️🛑️🛑️${actual} !== ${expected}`;
+//   if (actual === expected) {
+//     console.log(`Assertion Passed:✅️✅️✅️ ${actual} === ${expected}`);
+//   }
+//   console.assert(actual === expected, message);
+// };
+
+
+
+// module.exports = assertEqual;
